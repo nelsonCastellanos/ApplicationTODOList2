c94248e9f3f66d232e7858291d816aa75d6e5fc4
Nelson Castellanos <nelson.castellanos@mercadolibre.com.co>
2021-10-30 01:07:12 -0500

    - Upload google firebase to save data

--- app/src/main/java/com/umb/applicationtodolist/FirstFragment.java
+++ app/src/main/java/com/umb/applicationtodolist/FirstFragment.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.umb.applicationtodolist.databinding.FragmentFirstBinding;
+
+import java.util.Date;
 
 public class FirstFragment extends Fragment {
 
@@ -22,6 +31,22 @@ public class FirstFragment extends Fragment {
     ) {
 
         binding = FragmentFirstBinding.inflate(inflater, container, false);
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                long datePicker = binding.calendarView.getDate();
+                Date d = new Date(datePicker);
+                String asunto = binding.editTextTextPersonName.getText().toString();
+                String activity = binding.editTextTextMultiLine.getText().toString();
+                // Write a message to the database
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue("Hello, World!");
+
+                Snackbar.make(view, "Guardando Información", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         return binding.getRoot();
 
     }
